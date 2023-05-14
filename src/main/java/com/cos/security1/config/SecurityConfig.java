d6e6970df537cc3ab43376f5d836b1424711d9ee
ghtn9398 <ghtn9398@nate.com>
2023-05-14 17:10:46 +0900

    스프링 시큐리티 기본세팅 및 securityConfig 추가

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    protected void configure(HttpSecurity http)throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                //  /user/** 이하는 로그인 한사람만 들어올수있음
+                .antMatchers("/user/**").authenticated()
+                // /manager/** 이하는 로그인은 했지만 admin 이나 manager 권한이 있는사람만 들어올수 있다.
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') ")
+                // 위의경우를 제외한 경우는 모두 허용
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+
+    }
+
+
+}
