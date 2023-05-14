e97a46854f800c4daa1a809b41fbffddf3a6f49d
ghtn9398 <ghtn9398@nate.com>
2023-05-14 17:54:42 +0900

    PasswordEncoder 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.Filter;
 
@@ -12,6 +14,11 @@ import javax.servlet.Filter;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //Bean 어노테이션 선언시 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다
+    @Bean
+    public BCryptPasswordEncoder encoderPwd(){
+        return new BCryptPasswordEncoder();
+    }
 
 
     protected void configure(HttpSecurity http)throws Exception{
@@ -26,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm");
 
     }
 
