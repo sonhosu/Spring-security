bc121c9913d0764ed520df453b9d34efc9525b51
ghtn9398 <ghtn9398@nate.com>
2023-05-14 18:49:45 +0900

    UserRepository 에 findByUsername 추가

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.cos.security1.repository;
 import com.cos.security1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
 
+//CRUD 함수를 JpaRepository 가 들고있다.
+// @Repository 라는 어노테이션이 없어도 IoC 됨. 이유는 JpaRepository를 상속했기때문
+public interface UserRepository extends JpaRepository<User, Integer> {
+    //findBy 규칙 -> Username 문법
+    // select * from user where username = username(파라미터)
+    public User findByUsername(String username);
 
 }
